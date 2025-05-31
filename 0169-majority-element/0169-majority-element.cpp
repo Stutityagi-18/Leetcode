e41cc728fe80ef284065c9084b662cc093b9a23b
Stuti Tyagi <stutityagi18@gmail.com>
2025-05-31 17:05:23 +0530

    Time: 4 ms (19.54%), Space: 28.1 MB (84.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -20,21 +20,54 @@ public:
         //     return nums[i];
         // ************better approach*********
         
-        map<int,int>mpp;
-        int i;
+        // map<int,int>mpp;
+        // int i;
+        // for(i=0;i<n;i++)
+        // {
+        //     mpp[nums[i]]++;
+        // }
+        // //searching for majority element
+        // for(auto it:mpp)
+        // {
+        //     if(it.second > (n/2))
+        //     {
+        //         return (it.first);
+        //     }
+        // }
+        //most optimal approach
+        int cnt=0;
+        int el,i;
         for(i=0;i<n;i++)
         {
-            mpp[nums[i]]++;
-        }
-        //searching for majority element
-        for(auto it:mpp)
-        {
-            if(it.second > (n/2))
+            if(cnt==0)
             {
-                return (it.first);
+                cnt=1;
+                el=nums[i];
+            }
+            else if(nums[i]==el)
+            {
+                cnt++;
+            }
+            else{
+                cnt--;
             }
         }
+        int cnt1=0;
+        for(i=0;i<n;i++)
+        {
+            if(nums[i]==el)
+            {
+                cnt1++;
+            }
+        }
+        if(cnt1>(n/2))
+        {
+            return el;
+        }
+        else
+        {
         return -1;
+        }
         
         
     }

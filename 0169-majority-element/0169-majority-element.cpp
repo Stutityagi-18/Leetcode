e3e464abfe22c0c31d5299432cbb70da4894d269
Stuti Tyagi <stutityagi18@gmail.com>
2026-01-12 22:53:14 +0530

    Time: 3 ms (28.54%), Space: 28.3 MB (27.27%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,74 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // sort(nums.begin(),nums.end());
         int n=nums.size();
-        //  int count,i;
-        // // return nums[n/2];
-        // for( i=0;i<n;i++)
-        // {
-        //     count=0;
-        //     for(int j=0;j<n;j++)
-        //     {
-        //         if(nums[j]==nums[i])
-        //         {
-        //             count++;
-        //         }
-        //     }
-        // }
-        // if(count > (n/2))
-        //     return nums[i];
-        // ************better approach*********
-        
-        // map<int,int>mpp;
-        // int i;
-        // for(i=0;i<n;i++)
-        // {
-        //     mpp[nums[i]]++;
-        // }
-        // //searching for majority element
-        // for(auto it:mpp)
-        // {
-        //     if(it.second > (n/2))
-        //     {
-        //         return (it.first);
-        //     }
-        // }
-        //most optimal approach
-        int cnt=0;
-        int el,i;
-        for(i=0;i<n;i++)
+        int ans=0;
+        unordered_map<int,int>mpp;
+        for(int i:nums)
         {
-            if(cnt==0)
+            mpp[i]++;
+        }
+        int m=mpp.size();
+        for(auto& it:mpp)
+        {
+            if(it.second >(n/2))
             {
-                cnt=1;
-                el=nums[i];
-            }
-            else if(nums[i]==el)
-            {
-                cnt++;
-            }
-            else{
-                cnt--;
+                ans=it.first;
             }
         }
-        int cnt1=0;
-        for(i=0;i<n;i++)
-        {
-            if(nums[i]==el)
-            {
-                cnt1++;
-            }
-        }
-        if(cnt1>(n/2))
-        {
-            return el;
-        }
-        else
-        {
-        return -1;
-        }
-        
+        return ans;
         
     }
 };
